7c077a5d79976aae936199373e0765db53d73334
Thor Christensen <thor9636@elevcampus.dk>
2024-12-06 10:33:52 +0100

    first commit

---
+++ mouse jiggler.py
@@ -0,0 +1,33 @@
+import pyautogui
+import threading
+import time
+import random
+from pynput import keyboard
+
+move_mouse = False
+
+def wiggle_mouse():
+    while move_mouse:
+        x, y = pyautogui.position()
+        x += random.randint(-50, 50)
+        y += random.randint(-50, 50)
+        pyautogui.moveTo(x, y, duration=1)
+        time.sleep(5)
+
+def on_press(key):
+    global move_mouse
+    try:
+        if key == keyboard.Key.f5:
+            move_mouse = not move_mouse
+            if move_mouse:
+                print("Mouse jiggler started. Press F5 to stop.")
+                threading.Thread(target=wiggle_mouse).start()
+            else:
+                print("Mouse jiggler stopped. Press F5 to start.")
+    except AttributeError:
+        pass
+
+print("Press F5 to start/stop the mouse jiggler.")
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+listener.join()
